0313b4b7fe2ad1f6feec7e10bc2db6caf8dccab6
BafS <fabacrans@gmail.com>
2016-05-24 14:52:15 +0200

    Add some seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Wikir.Version
+alias Wikir.Article
+
+Wikir.Repo.insert!(%Version{title: "Test", content: "Test content"})
+Wikir.Repo.insert!(%Version{title: "Truc", content: "# un truc content"})
+# Wikir.Repo.insert!(%Version{title: "Test1", content: "Test content", article_id: 1, user_id: 1})
+
+Wikir.Repo.insert!(%Article{})
