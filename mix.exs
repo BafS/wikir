38c8108d8ae78f4e76593c0d49dda22f57823249
BafS <fabacrans@gmail.com>
2016-05-30 18:44:28 +0200

    Add earmark to render markdown

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Wikir.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.1.4"},
      {:mariaex, ">= 0.0.0"},
+     {:earmark, "~> 0.2.1"},
      {:phoenix_ecto, "~> 2.0"},
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
